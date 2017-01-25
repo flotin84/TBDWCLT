9d3149bcc64075209bd9c0e1622adddfa3258ae7
James Caldwell <jamesronaldcaldwell@gmail.com>
2017-01-25 13:10:58 -0600

    Added node class.

--- ExperimentFileTools/src/TempTestDriver.java
+++ ExperimentFileTools/src/TempTestDriver.java
@@ -3,11 +3,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TempTestDriver {
 
 	public static void main(String args[])
 	{
+		ArrayList<Node> nodes = new ArrayList();
+	
+		Node test = new Node("bob","path","path2");
+		nodes.add(test);
+		
+		System.out.println(test.getBinPath());
+		System.out.println(test.getLogPath());
+		System.out.println(test.getName());
+
+		
 		String filePath = "C:\\Users\\JRC\\TestExperimentFiles\\file_example.txt";
 		String[] wrongFiles = {"C:\\Users\\JRC\\TestExperimentFiles\\file_example2.txt","C:\\Users\\JRC\\TestExperimentFiles\\file_example3.txt","C:\\Users\\JRC\\TestExperimentFiles\\file_example4.txt"};
 		try {
